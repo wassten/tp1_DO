06b6bdd414fb7b4ea17345927677a63de9c42770
wassten <wassim.zouitene@efrei.net>
2022-06-13 20:32:09 +0200

    ajout front

--- tp1.py
+++ tp1.py
@@ -12,7 +12,7 @@ longitude=env['LONG']
 api_key=env['API_KEY']
 
 
-
+st.title('NoWeather')
 url = "http://api.openweathermap.org/data/2.5/weather?"
 url = url + "lat=" + lattitude + "&lon=" + longitude + "&appid=" + api_key
 response = requests.get(url)
